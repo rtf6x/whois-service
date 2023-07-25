10453065a2061a1602f702393b1a18bbdba69254
rtf6x <alex@rootfox.cc>
2023-07-25 13:11:18 +0400

    remove jquery

--- helpers/specialChars.js
+++ helpers/specialChars.js
@@ -1,4 +1,4 @@
-export default function specialChars(text) {
+module.exports = function specialChars(text) {
   return text.replace(/\0/g, '0').replace(/\\(.)/g, '$1').replace(/&/g, '')
       .replace(/</g, '').replace(/>/g, '').replace(/\\/g, '').replace(/\|/g, '').replace(/\//g, '')
       .replace(/"/g, '').replace(/'/g, '');
