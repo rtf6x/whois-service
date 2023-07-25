43bb58bf039fc8ce2eb6c9fe387087a6dea9fcda
rtf6x <alex@rootfox.cc>
2023-07-25 13:12:38 +0400

    remove jquery

--- helpers/specialChars.js
+++ helpers/specialChars.js
@@ -1,5 +1,13 @@
-module.exports = function specialChars(text) {
-  return text.replace(/\0/g, '0').replace(/\\(.)/g, '$1').replace(/&/g, '')
-      .replace(/</g, '').replace(/>/g, '').replace(/\\/g, '').replace(/\|/g, '').replace(/\//g, '')
-      .replace(/"/g, '').replace(/'/g, '');
+module.exports = function specialChars(text = '') {
+  return text
+      .replace(/\0/g, '0')
+      .replace(/\\(.)/g, '$1')
+      .replace(/&/g, '')
+      .replace(/</g, '')
+      .replace(/>/g, '')
+      .replace(/\\/g, '')
+      .replace(/\|/g, '')
+      .replace(/\//g, '')
+      .replace(/"/g, '')
+      .replace(/'/g, '');
 }
